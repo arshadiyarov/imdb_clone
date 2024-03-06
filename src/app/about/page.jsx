e7595595ce64285621535364b04a755f280da3b3
Armat Shadiyarov <shadiyarovarmat@gmail.com>
2024-03-06 18:04:41 +0500

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className={"max-w-6xl mx-auto p-3 space-y-4"}>
+      <div className={"text-2xl font-medium text-amber-600"}>About</div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
+        consequuntur, eaque error itaque maiores maxime nam nobis nulla numquam
+        perferendis quibusdam quisquam quo repudiandae, rerum!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad autem
+        delectus dolore, ducimus exercitationem nemo neque nulla soluta
+        suscipit. Architecto dolor eligendi facilis fuga harum itaque, laborum
+        magnam molestiae molestias odio pariatur praesentium quasi qui
+        repudiandae voluptate. Asperiores ea expedita ipsa ipsum laborum
+        molestiae natus neque provident quisquam repudiandae.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut culpa
+        cumque deserunt dolorem error esse eum facilis fugiat hic illo ipsa
+        labore laudantium magni modi molestias nisi nobis non odio omnis quae,
+        quaerat quasi quod reprehenderit unde voluptates! Animi autem nam
+        reiciendis totam vitae voluptatibus! A ad adipisci aliquid consequatur
+        cumque dolorum eos eum eveniet excepturi itaque iure molestiae nam natus
+        necessitatibus officiis porro praesentium quia sequi, totam ullam.
+        Aliquid dicta excepturi facere impedit ipsam laborum maiores maxime non
+        sapiente veniam. Amet architecto eveniet facere illum impedit maiores
+        minus nulla officia porro quia, quis temporibus.
+      </p>
+    </div>
+  );
+};
+
+export default About;
