9c3ffa5619ec350be55585fd31c79ec359c89dbe
Armat Shadiyarov <shadiyarovarmat@gmail.com>
2024-03-06 19:06:37 +0500

    created loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default async function Home({ searchParams }) {
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } },
   );
   const data = await res.json();
   if (!res.ok) {
