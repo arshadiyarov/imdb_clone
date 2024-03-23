6631758f4beb66e7263f62578a61520c4b87aa28
arshadiyarov <shadiyarovarmat@gmail.com>
2024-03-23 15:40:18 +0500

    first commit

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -7,7 +7,7 @@ const MoviePage = async ({ params }) => {
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`,
   );
   const movie = await res.json();
-  console.log(movie);
+
   return (
     <div className={"w-full"}>
       <div
